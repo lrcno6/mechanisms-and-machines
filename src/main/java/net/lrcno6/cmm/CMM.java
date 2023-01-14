48f55715a3a0902b643f6f92941cd4ff973aad85
lrcno6 <lrcno6@foxmail.com>
2023-01-15 01:44:43 +0800

    edit the template and add some resources

--- src/main/java/net/lrcno6/cmm/CMM.java
+++ src/main/java/net/lrcno6/cmm/CMM.java
@@ -1,8 +1,9 @@
-package com.example.modid;
+package net.lrcno6.cmm;
 
 import com.simibubi.create.Create;
 
 import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
+
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.ResourceLocation;
@@ -10,9 +11,9 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
-	public static final String NAME = "Example Mod";
+public class CMM implements ModInitializer {
+	public static final String ID = "cmm";
+	public static final String NAME = "Create: Mechanisms & Machines";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
 	@Override
